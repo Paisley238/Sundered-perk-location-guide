a8edb506d95872139615f8f2c98e738223316c1c
Arthur <ascofield@watkinson.org>
2026-05-28 09:16:33 -0400

    Changed entire website from grid to info on top 5 survival perks

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ function onPageLoaded() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Listen for clicks on elements with the class 'play-button'
     document.querySelectorAll(".play-button").forEach(function (button) {
         button.addEventListener("click", function () {
-            // When a play button is clicked, simulate a click on the <a> tag within the same .video-container
             this.parentNode.querySelector("a").click();
         });
     });
@@ -14,56 +12,21 @@ document.addEventListener("DOMContentLoaded", function () {
 const btn = card.querySelector(".perk-button");
 
 btn.addEventListener("click", () => {
-    // YOUR CODE HERE:
-    // How do you "toggle" the class 'open' on the 'card' element?
-    // Hint: in JS, it looks like element.classList.toggle('className');
-});
+    card.querySelector(".perk-button").addEventListener("click", (e) => {
+        e.stopPropagation();
 
+        card.classList.toggle("open");
+    });
+});
 btn.addEventListener("click", (event) => {
-    // 1. Stop the click from bubbling up (useful for "clicking off" later)
     event.stopPropagation();
-
-    // 2. YOUR CODE HERE:
-    // We want to add/remove the 'open' class on the 'card' element.
-    // Hint: card.classList._____('open');
 });
 
-// This part handles "clicking off" to close the info
 document.addEventListener("click", () => {
     card.classList.remove("open");
 });
 
-const perkNames = [
-    "Berzerk",
-    "Sharp Edge",
-    "Assassin",
-    "Bullet Transfabricator",
-    "Shield Piercer",
-    "High Density Bullets",
-    "Executioner",
-    "Deathblow",
-    "Life Absorb",
-    "Shield Absorb",
-    "Shield Converter",
-    "Shield Overdrive",
-    "Tough Skin",
-    "Stand Firm",
-    "Shield Flasks",
-    "Harvester",
-    "Shield Drain",
-    "Rebirth",
-    "Steel Spine",
-    "Blind Luck",
-    "Shard Collecter",
-    "Monstrous Lure",
-    "Golden Charm",
-    "Health Charm",
-    "Magnet",
-    "Nemesis",
-    "Energy Absorb",
-    "Gunslinger",
-    "Eradicator"
-];
+const perkNames = ["Berzerk", "Sharp Edge", "Assassin", "Bullet Transfabricator", "Shield Piercer"];
 
 const container = document.querySelector(".perk-container");
 
@@ -83,7 +46,7 @@ perkNames.forEach((name) => {
   `;
 
     card.querySelector(".perk-button").addEventListener("click", (e) => {
-        e.stopPropagation(); // Prevents the click from reaching the 'body'
+        e.stopPropagation();
         card.classList.toggle("open");
     });
 
